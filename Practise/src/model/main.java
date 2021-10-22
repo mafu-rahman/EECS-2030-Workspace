24c952c106e87813c6f77797393e57124e5db07e
Mahfuz Rahman <mafu@my.yorku.ca>
2021-10-21 21:51:11 -0400

    ntg

--- Practise/src/model/main.java
+++ Practise/src/model/main.java
@@ -4,8 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		int x = 3/2;
-		System.out.println(x);
+		
+		A obj1 = new A();
+		A obj2 = new A(1, 1.1, false);
+		B obj3 = new B();
+		B obj4 = new A(1, 1.1, true);
+		B obj5 = new B(1, 1.1, true);
+		B obj6 = new B(2, 2.2, false, 'f');
+		C obj7 = new C(2, 2.2, false, 'f');
+		C obj8 = new C(2, 2.2, false);
 	}
 
 }
